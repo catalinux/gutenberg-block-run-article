fb02b7a4b6a3cc0e49a2e32e3169cfde227a7ea5
catalinux <catalin@run.ro>
2024-05-13 15:53:07 +0000

    Build: (8b11dea) Update publish.yml

---
+++ index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks'), 'version' => 'bfdd80f25cf708dbede8');
---
+++ view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3eacd7bac2cc065480f7');
